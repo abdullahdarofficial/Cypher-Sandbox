b7c2f0bfec04451cf665e1f0abc0f542582535e6
Abdullah Dar <darabdullah380@gmail.com>
2024-09-15 04:45:13 +0500

    Time: 38 ms (68%), Space: 16.4 MB (65.65%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,12 +1,33 @@
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+        
+#         i = 1
+#         while i <= x:
+
+#             if (i * i) >= x:
+#                 if (i * i) == x:
+#                     return i
+#                 return i - 1
+#             i += 1
+#         return 0
+
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-        
-        i = 1
-        while i <= x:
+        if x < 2:
+            return x
 
-            if (i * i) >= x:
-                if (i * i) == x:
-                    return i
-                return i - 1
-            i += 1
-        return 0
+        low, high = 2, x // 2
+
+        while low <= high:
+            mid = (low + high) // 2
+            mid_sq = mid * mid
+
+            if mid_sq == x:
+                return mid
+            elif mid_sq < x:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return high
