504b9d7c3ae22ac1ab8544f827f1c6a4dfe811b2
Abdullah Dar <darabdullah380@gmail.com>
2024-10-06 15:57:00 +0500

    Time: 35 ms (66.35%), Space: 16.7 MB (22.12%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -39,13 +39,42 @@ class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
 
 
+        # graph = defaultdict(dict)
+
+        # for (a,b), value in zip(equations, values):
+        #     graph[a][b] = value
+        #     graph[b][a] = 1/value
+
+        
+        # def dfs(start, end, visited):
+        #     if start not in graph or end not in graph:
+        #         return -1.0
+        #     if start == end:
+        #         return 1.0
+            
+        #     visited.add(start)
+
+        #     for neighbor, value in graph[start].items():
+        #         if neighbor not in visited:
+        #             res = dfs(neighbor, end, visited)
+        #             if res != -1.0:
+        #                 return res * value
+
+        #     return -1.0
+
+        # result = []
+        # for start, end in queries:
+        #     result.append(dfs(start, end, set()))
+        # return result
+
+        
+        
         graph = defaultdict(dict)
 
-        for (a,b), value in zip(equations, values):
+        for (a,b) , value in zip(equations, values):
             graph[a][b] = value
             graph[b][a] = 1/value
 
-        
         def dfs(start, end, visited):
             if start not in graph or end not in graph:
                 return -1.0
@@ -65,5 +94,4 @@ class Solution:
         result = []
         for start, end in queries:
             result.append(dfs(start, end, set()))
-
         return result
