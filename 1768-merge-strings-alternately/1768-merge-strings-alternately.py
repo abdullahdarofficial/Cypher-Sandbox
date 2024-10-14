6ab864fdd11b8c62f1e22a9f239f48b0cd79b42b
Abdullah Dar <darabdullah380@gmail.com>
2024-10-14 10:30:51 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,13 +1,12 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        
-        size = min(len(word1), len(word2))
-        word = []
-        for i in range(size):
-            word.append(word1[i])
-            word.append(word2[i])
-        if size < len(word1):
-            word.append(word1[size:])
-        elif size < len(word2):
-            word.append(word2[size:])
-        return "".join(word)            
+         merged = []
+        i, j = 0, 0
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                merged.append(word1[i])
+                i += 1
+            if j < len(word2):
+                merged.append(word2[j])
+                j += 1
+        return ''.join(merged)
