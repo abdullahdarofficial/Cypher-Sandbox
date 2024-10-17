f02a3f636f6586f879ccc4cb758ab74e619b5e60
Abdullah Dar <darabdullah380@gmail.com>
2024-10-16 23:57:07 -0700

    Time: 35 ms (55.16%), Space: 16.6 MB (18.68%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,28 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        mapping = {
-            "2": ["a", "b", "c"],
-            "3": ["d", "e", "f"],
-            "4": ["g", "h", "i"],
-            "5": ["j", "k", "l"],
-            "6": ["m", "n", "o"],
-            "7": ["p", "q", "r", "s"],
-            "8": ["t", "u", "v"],
-            "9": ["w", "x", "y", "z"],
-        }
-
-        if len(digits) == 0:
+        if not digits:
             return []
+
+        phone_map = {
+            '2' : 'abc', '3' : 'def', '4' : 'ghi', '5' : 'jkl',
+            '6' : 'mno', '7' : 'pqrs', '8' : 'tuv', '9': 'wxyz'
+        }
 
         results = []
 
-        def backtracking(index, combinations):
-            if index == len(digits):
+        def backtrack(combinations, next_digits):
+            if len(next_digits) == 0:
                 results.append(combinations)
-                return
+            else:
+                for letter in phone_map[next_digits[0]]:
+                    backtrack(combinations + letter, next_digits[1:])
 
-            for letter in mapping[digits[index]]:
-                backtracking(index + 1, combinations + letter)
-
-        backtracking(0, "")
+        backtrack("", digits)
         return results
