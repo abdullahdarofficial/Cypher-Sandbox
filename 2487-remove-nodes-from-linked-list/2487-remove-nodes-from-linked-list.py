68a41b424bb1f942384d4175e859550ee11103dd
Abdullah Dar <darabdullah380@gmail.com>
2024-10-23 19:32:56 +0500

    Time: 2278 ms (5.05%), Space: 48.5 MB (99.38%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.py
@@ -5,21 +5,43 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = ListNode(0)
-        dummy = curr
-        dummy.next = head
-        nextptr = head
+        # curr = ListNode(0)
+        # dummy = curr
+        # dummy.next = head
+        # nextptr = head
 
-        while dummy.next:
-            while nextptr:
-                while dummy.next and dummy.next != nextptr and dummy.next.val < nextptr.val:
-                    dummy.next = dummy.next.next
-                nextptr = nextptr.next
-            if dummy.next:
-                dummy = dummy.next
-                if dummy.next == None:
-                    break
-                else:
-                    nextptr = dummy.next
+        # while dummy.next:
+        #     while nextptr:
+        #         while dummy.next and dummy.next != nextptr and dummy.next.val < nextptr.val:
+        #             dummy.next = dummy.next.next
+        #         nextptr = nextptr.next
+        #     if dummy.next:
+        #         dummy = dummy.next
+        #         if dummy.next == None:
+        #             break
+        #         else:
+        #             nextptr = dummy.next
 
-        return curr.next
+        # return curr.next
+
+
+        # Optimize it 
+
+        stack = []
+        curr = head
+
+        while curr:
+            while stack and stack[-1].val < curr.val:
+                stack.pop()
+            stack.append(curr)
+            curr = curr.next
+        
+        dummy = ListNode(0)
+        curr = dummy
+        while stack:
+            curr.next = stack.pop(0)
+            curr = curr.next
+
+        curr.next = None
+        return dummy.next
+
