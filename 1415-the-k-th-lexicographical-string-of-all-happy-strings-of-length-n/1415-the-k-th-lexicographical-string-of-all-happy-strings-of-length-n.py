9c13e98b5d492c6f5b4b62f97a15c3775ecfa1b1
Abdullah Dar <darabdullah380@gmail.com>
2024-10-25 05:36:17 +0500

    Time: 0 ms (100%), Space: 16.9 MB (48.14%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,23 +1,47 @@
 class Solution:
     def getHappyString(self, n: int, k: int) -> str:
         
-        strs = 'abc'
-        result = []
-        def makecomb(s, curr):
-            print(curr)
-            if len(curr) < n:
-                for char in s:
-                    if curr and char != curr[-1]:
-                        makecomb(s, curr + char)
-                    elif not curr:
-                        makecomb(s, curr + char)
-            else:
-                result.append(curr)
+        # strs = 'abc'
+        # result = []
+        # def makecomb(s, curr):
+        #     print(curr)
+        #     if len(curr) < n:
+        #         for char in s:
+        #             if curr and char != curr[-1]:
+        #                 makecomb(s, curr + char)
+        #             elif not curr:
+        #                 makecomb(s, curr + char)
+        #     else:
+        #         result.append(curr)
 
-        makecomb(strs, "")
-        result.sort()
+        # makecomb(strs, "")
+        # result.sort()
 
-        if len(result) >= k:
-            return result[k-1]
-        else:
+        # if len(result) >= k:
+        #     return result[k-1]
+        # else:
+        #     return ""
+
+        chars = "abc"
+
+        total = 3 * ( 2 ** (n - 1))
+
+        if k > total:
             return ""
+
+        result = []
+
+        first = (k-1)//(2**(n-1))
+        result.append(chars[first])
+
+        k = (k - 1) % (2 ** (n-1))
+
+        for i in range(n - 1):
+            prev_char = result[-1]
+
+            remaining_chars = [c for c in chars if c != prev_char]
+            next_char_index = k // (2 ** (n-i-2))
+            result.append(remaining_chars[next_char_index])
+            k %= (2 ** (n-i-2))
+
+        return ''.join(result)
