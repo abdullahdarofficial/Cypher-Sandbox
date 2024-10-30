14633dc5e1165e599d7d894171c0e4e78125d3ba
Abdullah Dar <darabdullah380@gmail.com>
2024-10-31 00:12:20 +0500

    Time: 11 ms (8.22%), Space: 16.8 MB (12.9%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.py
+++ 1629-slowest-key/1629-slowest-key.py
@@ -4,6 +4,7 @@ class Solution:
         index = 0
         keys = set()
         maxTime = releaseTimes[index]
+        keys.add(keysPressed[index])
         for i in range(1, len(releaseTimes)):
             if (releaseTimes[i] - releaseTimes[i-1]) > maxTime:
                 maxTime = (releaseTimes[i] - releaseTimes[i-1])
@@ -13,4 +14,4 @@ class Solution:
                 keys.add(keysPressed[i])
             print(keys)
 
-        return max(keys)
+        return max(keys) if keys else ''
