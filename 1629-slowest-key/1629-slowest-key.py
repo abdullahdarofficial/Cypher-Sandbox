1674b7744d690472c7ad68c97c46217c3c39ecb7
Abdullah Dar <darabdullah380@gmail.com>
2024-10-31 00:08:24 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.py
@@ -0,0 +1,16 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        
+        index = 0
+        keys = set()
+        maxTime = releaseTimes[index]
+        for i in range(1, len(releaseTimes)):
+            if (releaseTimes[i] - releaseTimes[i-1]) > maxTime:
+                maxTime = (releaseTimes[i] - releaseTimes[i-1])
+                keys.clear()
+                keys.add(keysPressed[i])
+            elif (releaseTimes[i] - releaseTimes[i-1]) == maxTime:
+                keys.add(keysPressed[i])
+            print(keys)
+
+        return max(keys)
