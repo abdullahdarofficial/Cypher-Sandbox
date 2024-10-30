e4f6fbe7c9f76679259fea6f556bea817ea59a89
Abdullah Dar <darabdullah380@gmail.com>
2024-10-31 00:12:28 +0500

    Time: 3 ms (41.1%), Space: 16.8 MB (12.9%) - LeetHub

--- 1629-slowest-key/1629-slowest-key.py
+++ 1629-slowest-key/1629-slowest-key.py
@@ -1,17 +1,28 @@
 class Solution:
     def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
         
-        index = 0
-        keys = set()
-        maxTime = releaseTimes[index]
-        keys.add(keysPressed[index])
+        # index = 0
+        # keys = set()
+        # maxTime = releaseTimes[index]
+        # keys.add(keysPressed[index])
+        # for i in range(1, len(releaseTimes)):
+        #     if (releaseTimes[i] - releaseTimes[i-1]) > maxTime:
+        #         maxTime = (releaseTimes[i] - releaseTimes[i-1])
+        #         keys.clear()
+        #         keys.add(keysPressed[i])
+        #     elif (releaseTimes[i] - releaseTimes[i-1]) == maxTime:
+        #         keys.add(keysPressed[i])
+        #     print(keys)
+
+        # return max(keys) if keys else ''
+
+        maxTime = releaseTimes[0]
+        slowest = keysPressed[0]
         for i in range(1, len(releaseTimes)):
             if (releaseTimes[i] - releaseTimes[i-1]) > maxTime:
                 maxTime = (releaseTimes[i] - releaseTimes[i-1])
-                keys.clear()
-                keys.add(keysPressed[i])
+                slowest = keysPressed[i]
             elif (releaseTimes[i] - releaseTimes[i-1]) == maxTime:
-                keys.add(keysPressed[i])
-            print(keys)
+                slowest = max(keysPressed[i], slowest)
 
-        return max(keys) if keys else ''
+        return slowest
