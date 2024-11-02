7e0078eae12d952d8964cabf280d3c0eead86d47
Abdullah Dar <darabdullah380@gmail.com>
2024-11-02 20:27:26 +0500

    Time: 0 ms (100%), Space: 18 MB (10.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}
+        num_to_index = {}  # Dictionary to store number and its index
         
         for i, num in enumerate(nums):
-            diff = target - num
-            if diff in num_dict:
-                return [num_dict[diff], i]
-            num_dict[num] = i
+            complement = target - num  # The number needed to reach the target
+            if complement in num_to_index:  # Check if complement is in dictionary
+                return [num_to_index[complement], i]  # Return indices of complement and current num
+            num_to_index[num] = i  # Store the index of the current number
