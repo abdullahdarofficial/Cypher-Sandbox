55ee2e9d5446c29b4758d85951139b63e317a1fb
Abdullah Dar <darabdullah380@gmail.com>
2024-11-03 15:36:52 +0500

    Time: 460 ms (25.89%), Space: 16.7 MB (13.41%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -1,22 +1,29 @@
 class Solution:
     def countBalls(self, lowLimit: int, highLimit: int) -> int:
-        def numsum(x):
-            result = 0
-            while x > 0:
-                result += x%10
-                x//=10
-            return result
+        # def numsum(x):
+        #     result = 0
+        #     while x > 0:
+        #         result += x%10
+        #         x//=10
+        #     return result
 
-        maxcount = float('-inf')
-        map = {}
+        # maxcount = float('-inf')
+        # map = {}
+        # for num in range(lowLimit, highLimit + 1):
+        #     boxNum = numsum(num)
+        #     print(boxNum)
+        #     if boxNum not in map:
+        #         map[boxNum] = 0
+        #     map[boxNum] += 1
+        #     maxcount = max(map[boxNum], maxcount)
+
+        # return maxcount
+            
+            
+
+            
+        counts = [0] * 82
         for num in range(lowLimit, highLimit + 1):
-            boxNum = numsum(num)
-            print(boxNum)
-            if boxNum not in map:
-                map[boxNum] = 0
-            map[boxNum] += 1
-            maxcount = max(map[boxNum], maxcount)
-
-        return maxcount
-            
-            
+            boxNum = sum(int(digit) for digit in str(num))
+            counts[boxNum] += 1
+        return max(counts)
