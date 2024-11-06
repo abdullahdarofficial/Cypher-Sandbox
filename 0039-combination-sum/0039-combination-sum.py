fcfd10f4cd3e042992f01d5a1f1d0a822bd34b54
Abdullah Dar <darabdullah380@gmail.com>
2024-11-06 23:31:16 +0500

    Time: 816 ms (5.01%), Space: 16.7 MB (65.48%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,15 +1,29 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        result = set()
+        # result = set()
+        # def comb(curr):
+        #     total = sum(curr)
+        #     if total >= target:
+        #         if total == target:
+        #             curr.sort()
+        #             result.add(tuple(curr))
+        #     else:
+        #         for num in candidates:
+        #             comb(curr + [num])
+        # comb([])
+        # return [list(num) for num in result]
+
+        result = []
         def comb(curr):
             total = sum(curr)
             if total >= target:
                 if total == target:
                     curr.sort()
-                    result.add(tuple(curr))
+                    if curr not in result:
+                        result.append(curr)
             else:
                 for num in candidates:
                     comb(curr + [num])
         comb([])
-        return [list(num) for num in result]
+        return result
