d101fc5a371e49f0dbe6819dd25059ce7b31d2cf
Abdullah Dar <darabdullah380@gmail.com>
2024-11-06 23:19:58 +0500

    Time: 807 ms (5.01%), Space: 16.8 MB (11.1%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        result = set()
+        def comb(curr):
+            total = sum(curr)
+            if total >= target:
+                if total == target:
+                    curr.sort()
+                    result.add(tuple(curr))
+            else:
+                for num in candidates:
+                    comb(curr + [num])
+        comb([])
+        return [list(num) for num in result]
