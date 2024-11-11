3b13c12f66ba1fdc285d7a8d1e8b0200282f4d7f
Abdullah Dar <darabdullah380@gmail.com>
2024-11-11 19:00:36 +0500

    Time: 0 ms (100%), Space: 16.6 MB (44.86%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,23 +1,24 @@
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        Do not return anything, modify nums1 in-place instead.
+        Merges two sorted arrays nums1 and nums2 in-place.
+        Modify nums1 in-place to contain the sorted elements.
         """
+        # Start from the end of both arrays
+        p1, p2 = m - 1, n - 1
+        # Start from the end of the nums1 array where it has extra space
+        p = m + n - 1
         
-        i = m - 1
-        j = n - 1
-        k = len(nums1) - 1
-        while j >= 0 and i >= 0:
-            if nums1[i] < nums2[j]:
-                nums1[k] = nums2[j]
-                j-=1
+        # While there are still elements to compare in nums2 and nums1
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[k] = nums1[i]
-                i-=1
-            k -= 1
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j-=1
-            k-=1
-        return nums1
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
 
+        # If there are still elements in nums2, copy them to nums1
+        nums1[:p2 + 1] = nums2[:p2 + 1]
