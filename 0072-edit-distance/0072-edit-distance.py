ff5fb14308b1f164e67c1b589af1907c3a622f53
Abdullah Dar <darabdullah380@gmail.com>
2024-11-13 19:04:38 +0500

    Time: 64 ms (59.07%), Space: 20.2 MB (64.42%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,23 +1,52 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        memo = {}
 
-        def recurrence(i, j):
-            if (i,j) in memo:
-                return memo[(i,j)]
+        # memo = {}
+        # def recurrence(i, j):
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
 
-            if i == len(word1):
-                return len(word2) - j
-            if j == len(word2):
-                return len(word1) - i
+        #     # base cases
 
-            if word1[i] == word2[j]:
-                return recurrence(i + 1, j + 1)
-            else:
-                return 1 + min(
-                    recurrence(i + 1, j),
-                    recurrence(i, j + 1),
-                    recurrence(i + 1, j + 1)
-                )
-            return memo[(i,j)]
-        return recurrence(0,0)
+        #     if i == len(word1):
+        #         return len(word2) - j
+        #     if j == len(word2):
+        #         return len(word1) - i
+
+        #     # recurrence case
+        #     if word1[i] == word2[j]:
+        #         return recurrence(i + 1, j + 1)
+        #     else:
+        #         return 1 + min(
+        #             recurrence(i + 1, j),
+        #             recurrence(i, j + 1),
+        #             recurrence(i + 1, j + 1)
+        #         )
+
+        #     return memo[(i,j)]
+
+        # return recurrence(0,0)
+
+        n = len(word1)
+        m = len(word2)
+
+        dp = [[0] * (m + 1) for _ in range(n + 1)]  
+
+        for i in range(n + 1):
+            dp[i][0] = i
+
+        for j in range(m + 1):
+            dp[0][j] = j
+
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + min(
+                        dp[i-1][j],
+                        dp[i][j-1],
+                        dp[i-1][j-1]
+                    )
+        return dp[n][m]
