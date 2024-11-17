0fe0672e7afb5e731b856f6e54955752335dd4f7
Abdullah Dar <darabdullah380@gmail.com>
2024-11-17 23:56:01 +0500

    Time: 225 ms (87.48%), Space: 19 MB (74.47%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,33 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        
+        n = len(grid)
+        m = len(grid[0])
 
-        m, n = len(grid), len(grid[0])
-        visited = [[False] * n for _ in range(m)]
-        num_islands = 0
+        def recursive(i, j):
+            if 0 <= i < n and 0 <= j < m and grid[i][j] == "1":
+                grid[i][j] = "2"
+                recursive(i + 1, j)
+                recursive(i - 1, j)
+                recursive(i, j + 1)
+                recursive(i, j - 1)
 
-        def bfs(i: int, j: int):
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
+                    recursive(i,j)
+                    count += 1
 
-            directions = [(-1,0), (1,0), (0,-1), (0,1)]
-
-            queue = deque([(i,j)])
-            visited[i][j] = True
-
-            while queue:
-                x, y = queue.popleft()
-
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-
-                    if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny] and grid[nx][ny] == '1':
-                        visited[nx][ny] = True
-                        queue.append((nx, ny))
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == '1' and not visited[i][j]:
-                    bfs(i, j)
-                    num_islands += 1
-
-        return num_islands
+        return count
