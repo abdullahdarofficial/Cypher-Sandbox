9a170cf677e280b28e75880b32560fa185d9c5d2
Abdullah Dar <darabdullah380@gmail.com>
2024-12-12 19:04:10 +0500

    Time: 0 ms (100%), Space: 18.4 MB (9.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,4 +6,5 @@ class Solution:
             complement = target - num  # The number needed to reach the target
             if complement in num_to_index:  # Check if complement is in dictionary
                 return [num_to_index[complement], i]  # Return indices of complement and current num 
-            num_to_index[num] = i  # Store the index of the current number
+            num_to_index[num] = i  # Store the index of the current number  
+            
