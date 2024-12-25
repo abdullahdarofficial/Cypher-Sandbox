6a8277dd1d9b0cc33d6e97f14d56fa53b1ac3bc5
Abdullah Dar <darabdullah380@gmail.com>
2024-12-25 06:56:40 +0500

    Time: 3 ms (72.2%), Space: 17.8 MB (5.34%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,6 +6,7 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
+        
         dummy_head = ListNode(0)  # Create a dummy head for the result linked list
         current = dummy_head  # Pointer to construct the new list
         carry = 0  # Initialize carry
@@ -24,6 +25,5 @@ class Solution:
             current = current.next
             if l1 is not None: l1 = l1.next
             if l2 is not None: l2 = l2.next
-
 
         return dummy_head.next  # Return the next node of the dummy head, which is the start of the result
