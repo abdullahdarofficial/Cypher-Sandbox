9339e93a9a4e5cacee38031f48d6718a71a853e7
Abdullah Dar <darabdullah380@gmail.com>
2025-06-27 20:11:56 +0500

    Time: 4 ms (41.5%), Space: 19.2 MB (10.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:    
          
+      
         num_map = {}
         for i, num in enumerate(nums):
             complement = target - num
